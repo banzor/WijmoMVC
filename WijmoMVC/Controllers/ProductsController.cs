62127dc36e2c8157acb666dd7e1159cd18cb1082
Chris Bannon <bannzor@gmail.com>
2011-03-31 16:58:00 -0400

    Added support for IE9 and Firefox 4 via Wijmo 1.1.5 and jQuery 1.5.1.
    Also added Products/Grid view that demonstrates using server-side pading and sorting via JSONResult.

--- WijmoMVC/Controllers/ProductsController.cs
+++ WijmoMVC/Controllers/ProductsController.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Linq.Dynamic;
 using System.Web.Mvc;
+using System.Collections.Generic;
 
 using WijmoMVC.Models;
 namespace WijmoMVC.Controllers
@@ -29,5 +29,63 @@ namespace WijmoMVC.Controllers
             }
             return View(categories);
         }
+
+        public ActionResult Grid()
+        {
+            return View();
+        }
+
+        public JsonResult GetRows()
+        {
+            //paging
+            string sPageSize = Request["paging[pageSize]"];
+            int pageSize = -1;
+            if (!string.IsNullOrEmpty(sPageSize))
+            {
+                pageSize = int.Parse(sPageSize);
+            }
+            string sPageIndex = Request["paging[pageIndex]"];
+            int pageIndex = -1;
+            if (!string.IsNullOrEmpty(sPageIndex))
+            {
+                pageIndex = int.Parse(sPageIndex);
+            }
+
+            //sorting
+            var sort = "";
+            var sortDir = "";
+            if (Request["sorting[0][dataKey]"] != null && Request["sorting[0][dataKey]"] != "")
+            {
+                sort = Request["sorting[0][dataKey]"];
+                if (Request["sorting[0][sortDirection]"] != null && Request["sorting[0][sortDirection]"] != "")
+                {
+                    sortDir = Request["sorting[0][sortDirection]"];
+                    if (sortDir.ToLower() == "ascending")
+                    {
+                        sort = "it." + sort + " asc";
+                    }
+                    else if (sortDir.ToLower() == "descending")
+                    {
+                        sort = "it." + sort + " desc";
+                    }
+                }
+            }
+            IEnumerable<ProductModel> products;
+            if (sort != "")
+            {
+                products = from p in northwind.Products.OrderBy(sort).Skip(pageIndex * pageSize).Take(pageSize) select new ProductModel() { ProductID = p.ProductID, ProductName = p.ProductName, UnitPrice = p.UnitPrice.Value };
+            }
+            else
+            {
+                products = from p in northwind.Products.OrderBy("it.ProductID").Skip(pageIndex * pageSize).Take(pageSize) select new ProductModel() { ProductID = p.ProductID, ProductName = p.ProductName, UnitPrice = p.UnitPrice.Value };
+            }
+            int totalCount = northwind.Products.Count();
+
+
+            var result = new { TotalRowCount = totalCount, Items = products };
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
---
+++ WijmoMVC/Models/Products.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WijmoMVC.Models
+{
+    public class ProductModel
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
